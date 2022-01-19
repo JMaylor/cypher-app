680185bb72f9a4970c197b051dacab1ca3c65a5f
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-01-19 14:41:42 +0000

    remove pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import "./main.css";
 import App from "./App.vue";
 import router from "./router";
@@ -7,7 +6,6 @@ import "@purge-icons/generated";
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
